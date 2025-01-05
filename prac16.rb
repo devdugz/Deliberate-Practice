29cd3e4c2e6b29e410b1870188fd35ac53f6b69f
realcdugz <connorduggan30@gmail.com>
2025-01-05 14:13:25 -0600

    Daily Practice

--- prac16.rb
+++ prac16.rb
@@ -1,7 +1,11 @@
-while true
-  puts "Enter a word: "
-  input = gets.chomp
-  if input == "stop"
-    break
-  end
+array1 = ["hello, goodbye"]
+array2 = []
+index = 0
+
+while index < array1.length
+  array = array1[index]
+  array2 << array.upcase
+  index = index + 1
 end
+
+puts array2
